e99227f084a6b728ab68eb9f076c6c544eb7090a
Aryeh Zucker <159961161+AryehZucker@users.noreply.github.com>
2025-05-06 19:34:19 -0400

    Add methods to check if a String is a name, integer, ancestor, and palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.regex.Pattern;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static Pattern properNamePattern = Pattern.compile("[A-Z][a-z]*"),
+            integerPattern = Pattern.compile("(\\+|-)?(0|[1-9]\\d*)(\\.\\d+)?"),
+            ancestorPattern = Pattern.compile("((great-)*grand)?(father|mother)"),
+            palindromePattern = Pattern.compile("([a-z])([a-z])([a-z])([a-z])([a-z])\\5\\4\\3\\2\\1");
+
+    public static boolean properName(String s) {
+        return properNamePattern.matcher(s).matches();
     }
+
+    public static boolean integer(String s) {
+        return integerPattern.matcher(s).matches();
+    }
+
+    public static boolean ancestor(String s) {
+        return ancestorPattern.matcher(s).matches();
+    }
+
+    public static boolean palindrome(String s) {
+        return palindromePattern.matcher(s.toLowerCase()).matches();
+    }
+
 }
--- tests/MainTest.java
+++ tests/MainTest.java
@@ -1,8 +1,56 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class MainTest {
 
-    @org.junit.jupiter.api.Test
-    void main() {
+    @ParameterizedTest
+    @ValueSource(strings = { "Bob", "Smith", "Joey" })
+    void testProperName(String name) {
+        assertTrue(Main.properName(name));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "bob", "smIth", "JoEy", "", "Bob " })
+    void testImproperName(String name) {
+        assertFalse(Main.properName(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "12", "43.23", "-34.5", "+98.7", "0", "0.0230" })
+    void testInteger(String number) {
+        assertTrue(Main.integer(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "023", "", " ", "12s", "0.", ".3" })
+    void testNonInteger(String number) {
+        assertFalse(Main.integer(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "father", "mother", "great-great-grandmother" })
+    void testAncestor(String name) {
+        assertTrue(Main.ancestor(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "", "foo" })
+    void testNonAncestor(String name) {
+        assertFalse(Main.ancestor(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "asdfggfdsa", "asdfGgfdSa" })
+    void testPalindrome(String palindrome) {
+        assertTrue(Main.palindrome(palindrome));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "asdfggfdss", "basdfggfdsab", "" })
+    void testNotPalindrome(String palindrome) {
+        assertFalse(Main.palindrome(palindrome));
+    }
+
 }
